2a8e7ff5c8262072a767f443e45621cb2572de0f
rikcav <henriquearau000@gmail.com>
2024-03-05 16:25:16 -0300

    feat: cadastro do DeleteUserIntentHandler

--- lambda/src/com/amazon/ask/helloworld/HelloWorldStreamHandler.java
+++ lambda/src/com/amazon/ask/helloworld/HelloWorldStreamHandler.java
@@ -17,6 +17,7 @@ import com.amazon.ask.helloworld.handlers.AskForDevicesIntentHandler;
 import com.amazon.ask.helloworld.handlers.AskForTipsIntentHandler;
 import com.amazon.ask.helloworld.handlers.AskForUserIntentHandler;
 import com.amazon.ask.helloworld.handlers.CancelandStopIntentHandler;
+import com.amazon.ask.helloworld.handlers.DeleteUserIntentHandler;
 import com.amazon.ask.helloworld.handlers.HelloWorldIntentHandler;
 import com.amazon.ask.helloworld.handlers.HelpIntentHandler;
 import com.amazon.ask.helloworld.handlers.SessionEndedRequestHandler;
@@ -33,6 +34,7 @@ public class HelloWorldStreamHandler extends SkillStreamHandler {
 						new AskForTipsIntentHandler(),
 						new AskForDevicesIntentHandler(),
 						new AskForUserIntentHandler(),
+						new DeleteUserIntentHandler(),
 						new HelpIntentHandler(),
 						new LaunchRequestHandler(),
 						new SessionEndedRequestHandler(),

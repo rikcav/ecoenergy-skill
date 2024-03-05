8a54cad401d2d9fefcfdf7d3ac93d34e012def4c
rikcav <henriquearau000@gmail.com>
2024-03-05 15:54:39 -0300

    fix: mudança no id do modelo do usuário

--- lambda/src/com/amazon/ask/helloworld/responses/Usuario.java
+++ lambda/src/com/amazon/ask/helloworld/responses/Usuario.java
@@ -1,15 +1,15 @@
 package com.amazon.ask.helloworld.responses;
 
 public class Usuario {
-	private Long id;
+	private String id;
 	private String nome;
 	private Double tarifa;
 
-	public Long getId() {
+	public String getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
+	public void setId(String id) {
 		this.id = id;
 	}
 
